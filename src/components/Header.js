8beeeb76019bd17fdcff331650d85f40afdb69c6
sanchitarora802 <sanchit.arora802@gmail.com>
2025-07-30 15:36:37 +0530

    Fix: contact form fixed with the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,9 @@
 import React, { useEffect, useState } from "react";
 import styled, { useTheme } from "styled-components";
 import StyledButton from "./StyledButton";
+import ClickAwayListener from "react-advanced-click-away";
+import ContactForm from "./ContactForm";
+import StyledDiv from "./StyledDiv";
 
 const HeaderWrapper = styled.header`
   display: flex;
@@ -25,14 +28,23 @@ const Logo = styled.a`
   text-decoration: none;
 `;
 
-const Header = ({ onClick }) => {
+const Header = ({}) => {
   const [isAtTop, setIsAtTop] = useState(true);
+  const [showForm, setShowForm] = useState(false);
   const theme = useTheme();
 
   useEffect(() => {
     const handleScroll = () => {
-      // Set state to true if scrollY is 0, otherwise false
-      setIsAtTop(window.scrollY === 0);
+      const scrollY = window.scrollY;
+      const pageHeight =
+        document.documentElement.scrollHeight - window.innerHeight;
+      const scrolledPercent = (scrollY / pageHeight) * 100;
+      if (scrolledPercent < 10) {
+        setIsAtTop(true);
+        setShowForm(false);
+      } else {
+        setIsAtTop(false);
+      }
     };
 
     // Add the event listener when the component mounts
@@ -63,10 +75,28 @@ const Header = ({ onClick }) => {
         $fontSize={"0.875rem"}
         $fontWeight={500}
         $hoverBackground={theme.colors.hover}
-        onClick={onClick}
+        onClick={() => {
+          setShowForm(true);
+        }}
       >
         Get in touch
       </StyledButton>
+
+      <ClickAwayListener
+        onClickAway={() => {
+          setShowForm(false);
+        }}
+      >
+        <StyledDiv
+          $position={"absolute"}
+          $zIndex={9999}
+          $top={"90%"}
+          $left={"55%"}
+          $minWidth={"40%"}
+        >
+          {showForm && <ContactForm />}
+        </StyledDiv>
+      </ClickAwayListener>
     </HeaderWrapper>
   );
 };
